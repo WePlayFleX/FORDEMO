cd13241d466181d7eca7cd0397fb6bafa4c803dd
WePlayFleX <alex.kamai@ya.ru>
2021-12-08 00:56:34 +0300

    Добавьте файлы проекта.

---
+++ FORDEMO.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		cout << i * i << " ";
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
